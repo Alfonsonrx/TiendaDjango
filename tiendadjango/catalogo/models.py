610962a081598e5ac18b74c6775766585b089fcc
damian alfonso contreras orellana <damian.contreras036@gmail.com>
2022-09-16 10:20:17 -0300

    Migraciones principales creadas

--- tiendadjango/catalogo/models.py
+++ tiendadjango/catalogo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    pub_date = models.DateTimeField()
+    mod_date = models.DateTimeField()
+    serial_num = models.IntegerField()
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    
+    def __str__(self):
+        return self.name
+    
+class Comment(models.Model):
+    prod_rating = models.DecimalField(max_digits=3, decimal_places=1)
+    prod_num = models.IntegerField()
+    pub_date = models.DateTimeField()
+    user_id = models.IntegerField()
+    text_comment = models.TextField()
--- tiendadjango/tiendadjango/settings.py
+++ tiendadjango/tiendadjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "catalogo.apps.CatalogoConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'tiendadjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'bddtienda',
+        # 'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://<username>:<password>@<atlas cluster>/<myFirstDatabase>?retryWrites=true&w=majority'
+        }
     }
 }
 
