c34c366a1c1f657e83d3d6ac9ceff4c1b726d087
damian alfonso contreras orellana <damian.contreras036@gmail.com>
2022-09-27 02:52:11 -0300

    Solucion secret key

--- tiendadjango/tiendadjango/settings.py
+++ tiendadjango/tiendadjango/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_key_generator import secret_key_generator
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ol70779lhukc9+d1y1yyqkgr03u*$rcwe^$zfen*i=&3t^cgc'
+SECRET_KEY = secret_key_generator.generate()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
